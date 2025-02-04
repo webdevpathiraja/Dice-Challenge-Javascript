dbd108e10bef442d4fa4cbf8437dd49abe07cb81
W R Aloka Pathiraja <w2052008@westminster.ac.uk>
2025-02-04 09:27:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,11 +13,20 @@ var randomRightImageSrc = "images/dice" + randomNumber2 + ".png"; // change imag
 document.querySelectorAll("img")[1].setAttribute("src", randomRightImageSrc); // change attribute src
 
 
+// Check if randomNumber1 is greater than randomNumber2
 if (randomNumber1 > randomNumber2) {
+    // If Player 1's number is higher, update the <h1> element to display "Player 1 Wins!"
     document.querySelector("h1").innerHTML = "Player 1 Wins!";
-} else if (randomNumber2 > randomNumber1) {
+} 
+// Check if randomNumber2 is greater than randomNumber1
+else if (randomNumber2 > randomNumber1) {
+    // If Player 2's number is higher, update the <h1> element to display "Player 2 Wins!"
     document.querySelector("h1").innerHTML = "Player 2 Wins!";
-} else {
+} 
+// If both numbers are equal
+else {
+    // Update the <h1> element to display "Draw!"
     document.querySelector("h1").innerHTML = "Draw!";
 }
+
 
